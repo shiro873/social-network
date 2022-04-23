f3a96c0e25639d0a4c6da479338b541f4312643e
Ridwan Shuvro <shuvro_r@hotmail.com>
2022-04-23 17:42:36 +0600

    create post front-end

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -13,6 +13,13 @@ contract SocialNetwork {
         address payable author
     );
 
+    event PostTipped(
+        uint id,
+        string content,
+        uint tipAmount,
+        address payable author
+    );
+
     struct Post {
         uint id;
         string content;
@@ -36,5 +43,18 @@ contract SocialNetwork {
         emit PostCreated(postCount, _content, 0, msg.sender);
     }
 
-    
+    function tipPost(uint _id) public payable {
+        // Fetch the post
+        Post memory _post = posts[_id];
+        // Fetch the author
+        address payable _author = _post.author;
+        // Pay the author by sending them Ether
+        address(_author).transfer(msg.value);
+        // Increment the tip amount
+        _post.tipAmount = _post.tipAmount + msg.value;
+        // Update the post
+        posts[_id] = _post;
+        // Trigger an event
+        emit PostTipped(postCount, _post.content, _post.tipAmount, _author);
+    }
 }
